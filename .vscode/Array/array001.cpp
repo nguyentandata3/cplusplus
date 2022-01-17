7f211cbc6e59ded8996f038ab5f2b6ec6ff3fce6
nguyentandata3 <nguyentandata3@gmail.com>
2022-01-17 21:38:49 +0700

    Dem chan

--- .vscode/Array/array001.cpp
+++ .vscode/Array/array001.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define MAX 100
 void nhap(int a[],int &n);
 void lietkeCacPhanTuLe(int a[],int n);
+int tinhTongCoBaoNhieuSoChan(int a[],int n);
 int main()
 {
     int a[MAX]; //khai bao mang (toi da)
@@ -23,7 +24,9 @@ int main()
     }
     cout<<endl;
     lietkeCacPhanTuLe(a,n);
-
+    cout<<endl;
+    cout<<"Co tat ca "<<tinhTongCoBaoNhieuSoChan(a,n)<<" so chan";
+    cout<<endl;
     return 0;
 }
 void nhap(int a[],int &n)
@@ -45,4 +48,16 @@ void lietkeCacPhanTuLe(int a[],int n)
         }
     }   
     cout<<endl;
+}
+int tinhTongCoBaoNhieuSoChan(int a[],int n)
+{
+    int tong = 0;
+    for(int i = 0;i < n;i++)
+    {
+        if (a[i]%2==0)
+        {
+            tong+=1;
+        }
+    }
+    return tong;
 }
