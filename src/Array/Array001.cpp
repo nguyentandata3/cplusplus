44d0790266e701aeba4d2344287db5beee647e2e
nguyentandata3 <nguyentandata3@gmail.com>
2022-01-19 21:35:31 +0700

    kiem tra ton tai so chinh phuong

--- src/Array/Array001.cpp
+++ src/Array/Array001.cpp
@@ -7,6 +7,7 @@ void nhapMang(int a[],int &n); //khai bao ham nhap
 void xuatMang(int a[],int n); //khai bao ham xuat
 bool laSoChinhPhuong(int a);//khai bao ham kiem tra
 void lietkeSoChinhPhuong(int a[], int n); //khai bao ham liet ke so chinh phuong
+bool tontaiSoChinhPhuong(int a[],int n); //khai bao ham kiem tra ton tai so chinh phuong
 int main()
 {
     //khai bao
@@ -15,6 +16,10 @@ int main()
     nhapMang(mang,n);
     xuatMang(mang,n);
     lietkeSoChinhPhuong(mang,n);
+    if(tontaiSoChinhPhuong(mang,n)==1)
+    {
+        cout<<"Co ton tai so chinh phuong";
+    }else cout<<"Khong ton tai so chinh phuong";
     return 0;
 }
 void nhapMang(int a[],int &n)//xay dung ham nhap
@@ -42,6 +47,7 @@ bool laSoChinhPhuong(int a) //xay dung ham kiem tra
     bool test = false;
     if (sqrt(a)==(int)sqrt(a))
     {
+        if (a!=1)
         test = true;
     }
     return test;
@@ -52,9 +58,22 @@ void lietkeSoChinhPhuong(int a[], int n) //xay dung ham liet ke so chinh phuong
     {
         if (laSoChinhPhuong(a[i])==1)
         {
-            if (a[i]!=1)
+
             cout<<a[i]<<" ";
         }
     }
     cout<<endl;
+}
+bool tontaiSoChinhPhuong(int a[],int n) //xay dung ham kiem tra ton tai so chinh phuong
+{
+    bool test = false;
+    for (int i = 0;i < n;i++)
+    {
+        if(laSoChinhPhuong(a[i])==true)
+        {
+            test = true;
+            break;
+        }
+    }
+    return test;
 }
