4e25f85d07e04a37c8e2125ea575dfcb08a6dbf2
nguyentandata3 <nguyentandata3@gmail.com>
2022-01-26 21:49:03 +0700

    sinh vien

--- src/Struct/PhanSo.cpp
+++ src/Struct/PhanSo.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 typedef struct PhanSo
 {
     int tuso;
@@ -8,13 +7,15 @@ typedef struct PhanSo
 };
 void nhapPhanSo(PhanSo &phanso);
 void xuatPhanSo(PhanSo phanso);
+void menu(PhanSo phanso);
 int main()
 {
     //khai bao
     PhanSo phanso;
     //xu ly
     nhapPhanSo(phanso);
-    xuatPhanSo(phanso);
+    //xuatPhanSo(phanso);
+    menu(phanso);
     return 0;
 }
 void nhapPhanSo(PhanSo &phanso)
@@ -39,3 +40,40 @@ void xuatPhanSo(PhanSo phanso)
         cout<<phanso.tuso<<" / ("<<phanso.mauso<<")"<<endl;
     }
 }
+void menu(PhanSo phanso)
+{
+    int select;
+    do
+    {    
+        cout<<"--------------Lua chon cua ban la--------------"<<endl;
+        cout<<"1. Nhap lai"<<endl;
+        cout<<"2. Xuat phan so"<<endl;
+        cout<<"3. Rut gon phan so"<<endl;
+        cin>>select;
+        switch(select)
+        {
+            case 1: 
+            {
+                nhapPhanSo(phanso);
+                break;
+            }
+            case 2:
+            {
+                xuatPhanSo(phanso);
+                break;
+            }
+            case 3:
+            {
+                cout<<"rut gon phan so."<<endl;
+                break;
+            }
+            default:
+            {
+                cout<<"nhap sai."<<endl;
+                break;
+            }
+        } cout<<"Nhap 1 de thoat chuong trinh. Nhan 2 de tiep tuc chuong trinh: ";
+        cin>>select;
+    } while(select!=1);
+}
+
--- src/Struct/sinhvien.cpp
+++ src/Struct/sinhvien.cpp
@@ -1,15 +1,87 @@
 
-typedef struct sinhvien
+
+#include <iostream>
+using namespace std;
+typedef struct SinhVien
 {
     string hoten;
     string gioitinh;
     int namsinh;
+    float toan;
+    float ly;
+    float hoa;
     float gpa;
 };
+void nhapSV(SinhVien &sinhvien); //khai bao ham nhap sv
+void xuatSV(SinhVien sinhvien); //khai bao ham xuat sv
+void menu(SinhVien sinhvien);
 int main()
-{
+{   
+    SinhVien sinhvien;
+    nhapSV(sinhvien);
+    menu(sinhvien);
     //khai bao
     //xu ly
     //tra ve
     return 0;
+}
+void nhapSV(SinhVien &sinhvien)// xay dung ham nhap sv
+{
+    cout<<"Ho va ten: ";
+    cin>>sinhvien.hoten;
+    cout<<"Gioi tinh: ";
+    cin>>sinhvien.gioitinh;
+    cout<<"Nam sinh: ";
+    cin>>sinhvien.namsinh;
+    cout<<"Diem toan: ";
+    cin>>sinhvien.toan;
+    cout<<"Diem ly: ";
+    cin>>sinhvien.ly;
+    cout<<"Diem hoa: ";
+    cin>>sinhvien.hoa;
+    sinhvien.gpa = (sinhvien.toan + sinhvien.ly + sinhvien.hoa) / 3;
+}
+void xuatSV(SinhVien sinhvien) //xay dung ham xuat sv
+{
+    cout<<"Ho va ten: "<<sinhvien.hoten<<endl;
+    cout<<"Gioi tinh: "<<sinhvien.gioitinh<<endl;
+    cout<<"Nam sinh: "<<sinhvien.namsinh<<endl;
+    cout<<"GPA = "<<sinhvien.gpa<<endl;
+}
+void menu(SinhVien sinhvien)
+{
+    int select;
+    do
+    { 
+        cout<<"----------Moi lua chon----------"<<endl;
+        cout<<"1. Nhap lai thong tin sinh vien"<<endl;
+        cout<<"2. Xuat thong tin sinh vien"<<endl;
+        cout<<"3. ...";
+        cin>>select;
+        switch(select)
+        {
+            case 1:
+            {
+                nhapSV(sinhvien);
+                break;
+            }
+            case 2:
+            {
+                xuatSV(sinhvien);
+                break;
+            }
+            case 3:
+            {
+                cout<<"...."<<endl;
+                break;
+            }
+            default:
+            {
+                cout<<"Nhap sai."<<endl;
+                break;
+            }
+        }
+        cout<<"Nhap 1 de thoat. Nhap 2 de tiep tuc chuong trinh."<<endl;
+        cin>>select;
+    } while (select!=1);
 }
