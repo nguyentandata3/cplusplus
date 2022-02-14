aad90278fa19ea878ea77f7098ba3956b1176362
nguyentandata3 <nguyentandata3@gmail.com>
2022-02-14 22:19:18 +0700

    bai 1

--- src/1000baitap/1.cpp
+++ src/1000baitap/1.cpp
@@ -1,16 +1,21 @@
 //Tính S(n) = 1 + 2 + 3 + … + n bằng C / C++
 #include <iostream>
 using namespace std;
+int tong(int n);
 int main()
 {
     int n;
-    int tong = 0;
     cout<<"n = ";
     cin>>n;
+    cout<<"tong = "<<tong(n);
+    return 0;
+}
+int tong(int n)
+{
+    int tong = 0;
     for (int i = 0; i <= n;i++)
     {
         tong+=i;
     }
-    cout<<"tong = "<<tong;
-    return 0;
+    return tong;
 }
