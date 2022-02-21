5cb77a27822daf530ade07c28807ce9a3220803f
nguyentandata3 <nguyentandata3@gmail.com>
2022-02-21 21:41:04 +0700

    mang 2 chieu

--- .vscode/0218/Mang2chieu.cpp
+++ .vscode/0218/Mang2chieu.cpp
@@ -6,11 +6,11 @@ void nhap(int a[MAX_ROW][MAX_COLUM], int &colum, int &row);
 void xuat(int a[MAX_ROW][MAX_COLUM], int colum, int row);
 int timMax(int a[MAX_ROW][MAX_COLUM], int colum, int row);
 int timMin(int a[MAX_ROW][MAX_COLUM], int colum, int row);
-int maxHang(int a[MAX_ROW][MAX_COLUM], int colum, int hangcantim); // tim max hang can tim (findrow)
-int maxCot(int a[MAX_ROW][MAX_COLUM], int row, int cotcantim); // tim max cot can tim (findcolum)
+int maxHang(int a[MAX_ROW][MAX_COLUM], int colum, int row, int hangcantim); // tim max hang can tim (findrow)
+int maxCot(int a[MAX_ROW][MAX_COLUM],int colum, int row, int cotcantim); // tim max cot can tim (findcolum)
 int maxDuongCheo(int a[MAX_ROW][MAX_COLUM], int colum, int row); //tim max duong cheo 
-int maxDuongCheoNguoc9(int a[MAX_ROW][MAX_COLUM], int colum, int row);
-
+int maxDuongCheoNguoc(int a[MAX_ROW][MAX_COLUM], int colum, int row);
+int maxTongHang(int a[MAX_ROW][MAX_COLUM], int colum, int row);
 int main()
 {
     int a[MAX_ROW][MAX_COLUM];
@@ -18,12 +18,16 @@ int main()
     int findrow = 3;
     int findcolum = 3;
     nhap(a,colum,row);
+    cout<<endl;
     xuat(a,colum,row);
+    cout<<endl;
     cout<<"max = "<<timMax(a,colum,row)<<endl;
     cout<<"min = "<<timMin(a,colum,row)<<endl;
-    cout<<"max Hang = "<<maxHang(a,colum,findrow);
-    cout<<"max Cot = "<<maxCot(a,colum,findcolum);
-    cout<<"max duong cheo = "<<maxDuongCheo(a,colum,row);
+    cout<<"max Hang = "<<maxHang(a,colum,row,findrow)<<endl;
+    cout<<"max Cot = "<<maxCot(a,colum,row,findcolum)<<endl;
+    cout<<"max duong cheo = "<<maxDuongCheo(a,colum,row)<<endl;
+    cout<<"max duong cheo phu = "<<maxDuongCheoNguoc(a,colum,row)<<endl;
+    cout<<"max Tong hang = "<<maxTongHang(a,colum,row)<<endl;
     return 0;
 }
 void nhap(int a[MAX_ROW][MAX_COLUM], int &colum, int &row)
@@ -36,9 +40,9 @@ void nhap(int a[MAX_ROW][MAX_COLUM], int &colum, int &row)
     {
         for (int j = 0; j < colum; j++)
         {
-            cout<<"a["<<i<<"]["<<j<<"]: ";
-            // cin>>a[i][j];
             a[i][j] = i + j;
+            // cin>>a[i][j];
+           
         }
     }
 }
@@ -48,6 +52,7 @@ void xuat(int a[MAX_ROW][MAX_COLUM], int colum, int row)
     {
         for (int j = 0; j < colum; j++)
         {
+            cout<<"a["<<i<<"]["<<j<<"]: ";
             cout<<a[i][j];
             cout<<"  ";
         }
@@ -87,7 +92,7 @@ int maxHang(int a[MAX_ROW][MAX_COLUM], int colum, int row, int hangcantim)
     }
     return max;
 }
-int maxCot(int a[MAX_ROW][MAX_COLUM], int row, int cotcantim)
+int maxCot(int a[MAX_ROW][MAX_COLUM], int colum, int row, int cotcantim)
 {
     int max = a[0][cotcantim];
     for(int i = 0; i < row; i++)
@@ -108,22 +113,35 @@ int maxDuongCheo(int a[MAX_ROW][MAX_COLUM], int colum, int row) //tim max duong 
     }
     return max;
 }
-int maxDuongCheoNguoc9(int a[MAX_ROW][MAX_COLUM], int colum, int row)
+int maxDuongCheoNguoc(int a[MAX_ROW][MAX_COLUM], int colum, int row)
+
 {
     int max = a[0][row - 1];
     for(int i = 0; i < row; i++)
     {
         for(int j = 0; j < colum; j++)
         {
-            if(i+j==(row-1) && max < a[i][j]) max = a[i][j];
+            if(i+j==(colum-1) && max < a[i][j]) max = a[i][j];
         }
     }
     return max;
 }
-// 2 6 9 5 8 3 1 a[0][6], a[1][5], a[2][4], a[3][3], a[4][2], a[5][1], a[6][0]
-// 6 9 2 1 4 6 2
-// 8 2 3 6 4 1 6
-// 9 8 7 1 6 5 3
-// 4 8 6 2 1 4 9 
-// 6 3 1 5 9 8 7
-// 6 8 1 0 1 6 4
+int maxTongHang(int a[MAX_ROW][MAX_COLUM], int colum, int row)
+{
+    int max = 0;
+    int tong[MAX_ROW];
+    for (int i = 0; i < row; i++)
+    {
+        tong[i] = 0;
+        for (int j = 0; j < colum; j++)
+        {
+            tong[i] += a[i][j];
+        }
+    }
+    for (int i = 0; i < row; i++)
+    {
+            if (max < tong[i]) max = tong[i];
+    }
+    return max;
+}
+
