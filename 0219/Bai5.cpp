0a1e21c2e8e7f0c13189aa5a1facd78a369f4c61
nguyentandata3 <nguyentandata3@gmail.com>
2022-02-22 14:11:02 +0700

    bai tap giay

--- 0219/Bai5.cpp
+++ 0219/Bai5.cpp
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <conio.h>
 #define MAX 50
-
+int soluongthuc = 0;
 struct giay
 {
     char id[10];
@@ -11,11 +11,11 @@ struct giay
     int soluong;
     int dongia;
 };
-void nhapgiay(giay &giaydep[MAX], int &soluong)
+void nhapgiay(giay giaydep[MAX], int &soluongthuc)
 {   
     printf("Ban muon nhap bao nhieu giay: ");
-    scanf("%d",&soluong);
-    for(int i = 0; i < soluong; i++)
+    scanf("%d",&soluongthuc);
+    for(int i = 0; i < soluongthuc; i++)
     {
         printf("Thong tin giay thu %d: \n",i+1);
         printf("id: ");
@@ -30,10 +30,10 @@ void nhapgiay(giay &giaydep[MAX], int &soluong)
         scanf("%d",giaydep[i].dongia);
     }
 }
-void xuatgiay(giay giaydep[MAX], int soluong)
+void xuatgiay(giay giaydep[MAX], int soluongthuc)
 {
     printf("%15s%15s%10s%15s%20s\n","id","Size","Gioi tinh","So luong","Don gia");
-    for(int i = 0; i < soluong; i++)
+    for(int i = 0; i < soluongthuc; i++)
     {
         char gioitinh[10];
         if (giaydep[i].gioitinh == 0) strcpy(gioitinh,"nam");
@@ -42,14 +42,14 @@ void xuatgiay(giay giaydep[MAX], int soluong)
         printf("%15s%15s%10s%15s%20s\n",giaydep[i].id,giaydep[i].size,giaydep[i].gioitinh,giaydep[i].soluong,giaydep[i].dongia);
     }
 }
-giay timgiay(giay giaydep[MAX], int soluong)
+giay timgiay(giay giaydep[MAX], int soluongthuc)
 {
     giay giaycantim;
     printf("Ban muon tim giay gioi tinh nao?(0. Danh cho nu, 1. Danh cho nam, 2. Unisex): ");
     scanf("%d",giaycantim.gioitinh);
     printf("Size giay la: ");
     scanf("%f",giaycantim.size);
-    for(int i = 0; i < soluong; i++)
+    for(int i = 0; i < soluongthuc; i++)
     {
         if (giaycantim.gioitinh == giaydep[i].gioitinh && giaycantim.size == giaydep[i].size)
         {
@@ -58,42 +58,91 @@ giay timgiay(giay giaydep[MAX], int soluong)
     }
     return giaycantim;
 }
+void muagiay(giay giaydep[MAX], int soluongthuc)
+{
+    giay giaymuonmua;
+    int test;
+    printf("Moi nhap id giay ban muon mua: ");
+    gets(giaymuonmua.id);
+    for(int i = 0; i < soluongthuc; i++)
+    {
+        test = stricmp(giaymuonmua.id,giaydep[i].id);
+        if (test == 0)
+        {
+            char gioitinh[10];
+            if (giaydep[i].gioitinh == 0) strcpy(gioitinh,"nam");
+            else if (giaydep[i].gioitinh == 1) strcpy(gioitinh,"nu");
+            else strcpy(gioitinh, "Unisex");
+            printf("Giay muon mua la: \n");
+            printf("%15s%10f%15s%15d%20d\n",giaymuonmua.id,giaymuonmua.size,gioitinh,giaymuonmua.soluong,giaymuonmua.dongia);
+            break;
+        }  
+    }
+
+}
 int main()
 {
     giay giaydep[MAX];
-    giay timgiay;
-    int soluongthuc = 0;
-    void nhapgiay(giaydep,soluongthuc);
-    void xuatgiay(giaydep,soluongthuc);
-   
+    giay giaycantim;
+    giay giaymuonmua;
+    int test;
+    // void nhapgiay(giaydep,soluongthucthuc);
+    // void xuatgiay(giaydep,soluongthucthuc);
+    printf("Ban muon nhap bao nhieu giay: ");
+    scanf("%d",&soluongthuc);
+    for(int i = 0; i < soluongthuc; i++)
+    {
+        while (getchar () != '\n');
+        printf("Thong tin giay thu %d: \n",i+1);
+        printf("id: ");
+        gets(giaydep[i].id);
+        printf("size: ");
+        scanf("%f",&giaydep[i].size);
+        printf("gioi tinh (0. Danh cho nu, 1. Danh cho nam, 2. Unisex): ");
+        scanf("%d",&giaydep[i].gioitinh);
+        printf("So luong: ");
+        scanf("%d",&giaydep[i].soluong);
+        printf("Don gia: ");
+        scanf("%d",&giaydep[i].dongia);
+    }
+    printf("%15s%10s%15s%15s%20s\n","id","Size","Gioi tinh","So luong","Don gia");
+    for(int i = 0; i < soluongthuc; i++)
+    {
+        char gioitinh[10];
+        if (giaydep[i].gioitinh == 0) strcpy(gioitinh,"nam");
+        else if (giaydep[i].gioitinh == 1) strcpy(gioitinh,"nu");
+        else strcpy(gioitinh, "Unisex");
+        printf("%15s%10f%15s%15d%20d\n",giaydep[i].id,giaydep[i].size,gioitinh,giaydep[i].soluong,giaydep[i].dongia);
+    }
+    printf("Giay can tim la: \n");
+    printf("Ban muon tim giay gioi tinh nao?(0. Danh cho nu, 1. Danh cho nam, 2. Unisex): ");
+    scanf("%d",giaycantim.gioitinh);
+    printf("Size giay la: ");
+    scanf("%f",giaycantim.size);
+    for(int i = 0; i < soluongthuc; i++)
+    {
+        if (giaycantim.gioitinh == giaydep[i].gioitinh && giaycantim.size == giaydep[i].size)
+        {
+            giaycantim = giaydep[i];
+        }
+    }
+    printf("Moi nhap id giay ban muon mua: ");
+    gets(giaymuonmua.id);
+    for(int i = 0; i < soluongthuc; i++)
+    {
+        test = stricmp(giaymuonmua.id,giaydep[i].id);
+        if (test == 0)
+        {
+            char gioitinh[10];
+            if (giaydep[i].gioitinh == 0) strcpy(gioitinh,"nam");
+            else if (giaydep[i].gioitinh == 1) strcpy(gioitinh,"nu");
+            else strcpy(gioitinh, "Unisex");
+            printf("Giay muon mua la: \n");
+            printf("%15s%10f%15s%15d%20d\n",giaymuonmua.id,giaymuonmua.size,gioitinh,giaymuonmua.soluong,giaymuonmua.dongia);
+            break;
+        }  
+    }
     return 0;
 }
 
-    // printf("Ban muon nhap bao nhieu giay: ");
-    // scanf("%d",&soluongthuc);
-    // for(int i = 0; i < soluongthuc; i++)
-    // {
-    //     while (getchar () != '\n');
-    //     printf("Thong tin giay thu %d: \n",i+1);
-    //     printf("id: ");
-    //     gets(giaydep[i].id);
-    //     printf("size: ");
-    //     scanf("%f",&giaydep[i].size);
-    //     printf("gioi tinh (0. Danh cho nu, 1. Danh cho nam, 2. Unisex): ");
-    //     scanf("%d",&giaydep[i].gioitinh);
-    //     printf("So luong: ");
-    //     scanf("%d",&giaydep[i].soluong);
-    //     printf("Don gia: ");
-    //     scanf("%d",&giaydep[i].dongia);
-    // }
-    // printf("%15s%10s%15s%15s%20s\n","id","Size","Gioi tinh","So luong","Don gia");
-    // for(int i = 0; i < soluongthuc; i++)
-    // {
-    //     char gioitinh[10];
-    //     if (giaydep[i].gioitinh == 0) strcpy(gioitinh,"nam");
-    //     else if (giaydep[i].gioitinh == 1) strcpy(gioitinh,"nu");
-    //     else strcpy(gioitinh, "Unisex");
-    //     printf("%15s%10f%15s%15d%20d\n",giaydep[i].id,giaydep[i].size,gioitinh,giaydep[i].soluong,giaydep[i].dongia);
-    // }
-    // timgiay = timgiay(giaydep, 5);
-    // printf("Giay can tim la: \n");
+    
---
+++ 0221/Stundent.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class Student //animal, people,... 
+{
+    private: //access modifier
+    //properties
+    //du lieu
+        int id;
+        string name;
+        float gpa;
+    //method 
+    public: 
+        Student();//costructor
+};
+int main()
+{
+
+    return 0;
+}
