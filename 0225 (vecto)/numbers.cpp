62188419f5cfbbbefec5d120fcd42c29830fe31c
nguyentandata3 <nguyentandata3@gmail.com>
2022-02-25 22:11:00 +0700

    duyet mang kieu vector

--- 0225 (vecto)/numbers.cpp
+++ 0225 (vecto)/numbers.cpp
@@ -10,6 +10,11 @@ int main()
     numbers.push_back(2);// them 2 vao numbers[1] -> numbers[1] = 2
     numbers.push_back(3);// them 3 vao numbers[2] -> numbers[2] = 3
     numbers.pop_back(); //loai bo phan tu cuoi mang
-    cout<<numbers.size(); // xuat mang co bao nhieu phan tu
+    // cout<<numbers.size(); // xuat mang co bao nhieu phan tu
+    //duyet mang kieu vector
+    for(int number:numbers)
+    {
+        cout<<number<<" ";
+    }
     return 0;
 }
