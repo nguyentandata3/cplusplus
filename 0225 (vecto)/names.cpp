dfc76712655ee9545d1d0a7211b07eeca04bee3c
nguyentandata3 <nguyentandata3@gmail.com>
2022-02-25 22:25:08 +0700

    vector names

---
+++ 0225 (vecto)/names.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<string> names;
+    names.push_back("Minh Anh");
+    names.push_back("Tuan Kiet");
+    names.push_back("Tho Tho");
+    names.push_back("Son");
+    names.push_back("Dat"); 
+    if (!names.empty())
+    {
+        for(string name:names)
+        {
+            cout<<name<<" ";
+        }
+    } else cout<<"vector rong.";
+    return 0;
+}
