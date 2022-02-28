a7169018c60759f6c685daf795325a7654858a0e
nguyentandata3 <nguyentandata3@gmail.com>
2022-02-28 21:09:59 +0700

    huong doi tuong nhap sv

--- OOP/sinhvien.cpp
+++ OOP/sinhvien.cpp
@@ -10,6 +10,7 @@ class Student
         float gpa;
     public:
         Student(); // ham mac dinh
+        void nhap();
         Student(string name, string id, float gpa); // co tham so
         void xuat();
 };
@@ -19,6 +20,15 @@ Student::Student()
     this->id = "error";
     this->gpa = 0;
 }
+void Student::nhap()
+{
+    cout<<"Name: ";
+    cin>>name;
+    cout<<"Id: ";
+    cin>>id;
+    cout<<"GPA: ";
+    cin>>gpa;
+}
 Student::Student(string name, string id, float gpa)
 {
     this->name = name;
@@ -27,13 +37,17 @@ Student::Student(string name, string id, float gpa)
 }
 void Student::xuat()
 {
-    cout<<"Name: "<<name<<endl<<"Id: "<<id<<endl<<"GPA: "<<gpa<<endl;
+    cout<<"Name: "<<name<<" - Id: "<<id<<" - GPA: "<<gpa<<endl;
 }
 int main()
 {
     Student sv1;
     Student sv2("Hoang Hieu","ABC123",8.5);
+    Student sv3;
     sv1.xuat();
     sv2.xuat();
+    cout<<"Nhap sv thu 3."<<endl;
+    sv3.nhap();
+    sv3.xuat();
     return 0;
 }
