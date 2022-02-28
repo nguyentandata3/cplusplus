562e04902d74b913630f40b2476ae985319253aa
nguyentandata3 <nguyentandata3@gmail.com>
2022-02-28 22:18:35 +0700

    danh sach lines

--- OOP/Point.cpp
+++ OOP/Point.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<cmath>
 using namespace std;
 class Point{
     // dữ liệu
@@ -10,17 +11,47 @@ class Point{
     // khoi tao
         Point(); // mac dinh
         Point(int x, int y);// co tham so
+        void setx(int x)
+        {
+            this->x = x;
+        };
+        int getx()
+        {
+            return x;
+        };
+        void sety(int y)
+        {
+            this-> y = y;
+        };
+        int gety()
+        {
+            return y;
+        };
         void xuat();
 };
+class Line{
+    private:
+        Point first, second;
+    public:
+        Line();
+        Line(Point first, Point second);
+        float length();
+};
+class LineList{
+    private:
+        vector<Line> lines;
+    public:
+        void addLine(Line line);
+        void print();
+};
 int main(){
-    vector<Point> points;
-    points.push_back(Point(2,3));
-    points.push_back(Point(3,5));
-    points.push_back(Point(7,8));
-    for(int i = 0; i < points.size();i++)
-    {
-        points[i].xuat();
-    }
+    LineList lines;
+    Point A(3,4), B(2,3),C(4,2);
+    Line AB(A,B),AC(A,C),BC(B,C);
+    lines.addLine(AB);
+    lines.addLine(AC);
+    lines.addLine(BC);
+    lines.print();
     return 0;
 }
 Point::Point(){
@@ -33,4 +64,21 @@ Point::Point(int x, int y){
 }
 void Point::xuat(){
     cout<<"("<<x<<","<<y<<")";
+}
+Line::Line(){   
+}
+Line::Line(Point first,Point second){
+    this->first = first;
+    this->second = second;
+}
+float Line::length(){
+    return sqrt(pow(second.getx()-first.getx(),2) + (pow(second.gety()-first.gety(),2)));
+}
+void LineList::addLine(Line line){
+    this->lines.push_back(line);
+}
+void LineList::print(){
+    for(int i=0; i<lines.size();i++){
+        cout<<lines[i].length()<<" ";
+    }
 }
