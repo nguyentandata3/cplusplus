3a6ba44d1d285f0ea033e2a4cdc9a030f2872781
nguyentandata3 <nguyentandata3@gmail.com>
2022-03-02 20:19:54 +0700

    xuat employee

--- OOP/Employee.cpp
+++ OOP/Employee.cpp
@@ -13,19 +13,26 @@ class employee
         employee(); //ham mac dinh
         employee(string id, string name, int salary); //ham co tham so
         void nhap();
+        void xuat();
 };
 int main()
 {
     vector<employee> nv;
     int n;
+    employee employee;
     cout<<"ban muon nhap bao nhieu nv: ";
     cin>>n;
     for(int i = 0; i < n;i++)
     {
-        cout<<"nv thu ["<<i<<"]: ";
-        employee employee;
+        cout<<"nv thu ["<<i<<"]: "<<endl;
         employee.nhap();
         nv.push_back(employee);
+       
+    }
+    for(int i = 0; i < n; i++)
+    {
+        cout<<"thong tin nv thu ["<<i+1<<"]: "<<endl;
+        nv[i].xuat();
     }
     return 0;
 }
@@ -43,10 +50,17 @@ employee::employee(string id, string name, int salary)
 }
 void employee::nhap()
 {
-        // cout<<"id: ";
-        // cin>>id;
+        cout<<"id: ";
+        cin>>id;
         cout<<"name: ";
         cin>>name;
         cout<<"salary: ";
         cin>>salary;
+}
+void employee::xuat()
+{
+    this->name = name;
+    this->id = id;
+    this->salary = salary;
+    cout<<"Id: "<<id<<"     Name: "<<name<<"       Salary: "<<salary<<endl;
 }
