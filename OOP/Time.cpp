5d5cdc60ddc1b21fb3014f89a0711b37f22dc4df
nguyentandata3 <nguyentandata3@gmail.com>
2022-03-04 21:48:25 +0700

    them giay

--- OOP/Time.cpp
+++ OOP/Time.cpp
@@ -22,16 +22,18 @@ class Time
         void setphut(int phut);
         int getphut();
         void setgiay(int giay);
-        int getgiay();    
+        int getgiay();
+        void themGiay(int giay);    
 };
 int main()
 {
     Time t1,t2;
     Time t3(3601);
-    t2.nhap();
-    t2.xuat();
+    int them;
+    cout<<"So giay muon them: ";
+    cin>>them;
+    t3.themGiay(them);
     t3.xuat();
-    cout<<t2.getgio();
     return 0;
 }
 Time::Time()
@@ -44,7 +46,7 @@ Time::Time(int giay) // 61 --> 0:1:1
 {
     this->gio = giay/3600;
     this->phut = (giay - this->gio*3600)/60;
-    this->giay = giay - this->gio * 3600 - this->phut * 60;
+    this->giay = giay % 60;
 }
 Time::Time(int gio, int phut, int giay)
 {
@@ -63,6 +65,7 @@ void Time::nhap()
 }
 void Time::xuat()
 {
+    
     cout<<gio<<":"<<phut<<":"<<giay<<endl;    
 }
 void Time::setgio(int gio)
@@ -88,4 +91,14 @@ void Time::setgiay(int giay)
 int Time::getgiay()
 {
     return giay;
+}
+void Time::themGiay(int giay)
+{
+    // this->giay += giay;
+    // Time(this->giay);
+    giay = giay + (this->gio*3600 + this->phut*60 + this->giay);
+    this->gio = giay/3600;
+    this->phut = (giay - this->gio*3600)/60;
+    this->giay = giay % 60;
+    // *this = Time(giay);// *this: giá trị của thời gian
 }
