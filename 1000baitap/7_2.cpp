bdecabd2ebf55e64969b2f57e2168b9f1c11c05f
nguyentandata3 <nguyentandata3@gmail.com>
2022-03-07 20:49:51 +0700

    tim max (?)

---
+++ 1000baitap/7_2.cpp
@@ -0,0 +1,14 @@
+// tim max a,b
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,max;
+    cout<<"a = ";
+    cin>>a;
+    cout<<"b = ";
+    cin>>b;
+    max = a>b?a:b;
+    cout<<"max = "<<max<<endl;
+    return 0;
+}
