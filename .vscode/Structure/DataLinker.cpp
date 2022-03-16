5025c98bc9af26ccba44d790672d666991e15ea4
nguyentandata3 <nguyentandata3@gmail.com>
2022-03-16 21:26:05 +0700

    Node - DataLinker

---
+++ .vscode/Structure/DataLinker.cpp
@@ -0,0 +1,65 @@
+// [head]-[]-[]-[tail]
+#include <iostream>
+using namespace std;
+class Node
+{
+    private: 
+        int data;
+    public:
+        Node *next; // móc xích
+        void setdata(int data){this->data=data;}
+        int getdata(){return this->data;}
+};
+class List
+{
+    public:
+        Node *head, *tail;
+        List(){head = NULL;}
+        void addNode(Node *node);
+        void print();
+};
+void List::addNode(Node *node)
+{
+    if(!head)//head khac NULL
+    {
+        head = node;
+        head->next = NULL;
+    }
+    else
+    {
+        Node *current = head;
+        while(current->next)// chuyển đến node cuối cùng
+        {
+            current = current->next;
+        }
+        current-> next = node;
+    }
+}
+
+void List::print()
+{
+    Node *current = head;
+    while(current)
+    {
+        cout<<current->getdata()<<" ";
+        current = current->next;
+    }
+}
+
+int main()
+{
+    List *list = new List();
+    Node *node1, *node2, *node3;
+    node1 = new Node();    
+    node2 = new Node();
+    node3 = new Node();
+
+    node1->setdata(4);
+    node2->setdata(6);
+    node3->setdata(12);
+    list->addNode(node1);
+    list->addNode(node2);
+    list->addNode(node3);
+    list->print();
+    return 0;
+}
