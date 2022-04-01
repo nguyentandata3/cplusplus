35354ead0a209114dbf2852440cfdd660ce93d1f
nguyentandata3 <nguyentandata3@gmail.com>
2022-04-01 20:50:44 +0700

    thu vien array

--- .vscode/STL/array.cpp
+++ .vscode/STL/array.cpp
@@ -3,9 +3,10 @@
 using namespace std;
 int main()
 {
-    array<int,10> a={1,2,3,4,5,6,7,8,9,10};
+    array<int,9> a={1,2,3,4,5,6,7,8,9};
+    array<int,9> b={11,12,13,14,15,16,17,18,19};
     cout<<"using at(): ";
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 9; i++)
     {
         cout<<a.at(i)<<" ";
     }
@@ -14,12 +15,34 @@ int main()
     cout<<"element using get(): ";
     cout<<get<0>(a)<<" "<<get<1>(a)<<" "<<"\n";
     cout<<"using operator: ";
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < 9; i++)
     {
         cout<<a[i]<<" ";
     }
     cout<<"\n";
     cout<<"using front & back: ";
     cout<<a.front()<<" "<<a.back()<<"\n";
+    cout<<"using size(): "<<a.size()<<"\n";
+    cout<<"using max_size(): "<<a.max_size()<<"\n";
+    a.swap(b);
+    cout<<"sau swap: "<<"\n";
+    for(int i = 0; i < 9; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<"\n";
+    for(int i = 0; i < 9; i++)
+    {
+        cout<<b[i]<<" ";
+    }
+    cout<<"\n";
+    cout<<"using empty(): "<<a.empty()<<"\n";
+    cout<<"using fill(): ";
+    a.fill(0);
+    for(int i = 0; i < 9; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<"\n";
     return 0;
 }
