0aa3a9d875f64df6fd0a56371192ec4c0a75876c
nguyentandata3 <nguyentandata3@gmail.com>
2022-04-04 23:13:49 +0700

    sap xep sort

--- .vscode/STL/Pet_Project/thamlam.cpp
+++ .vscode/STL/Pet_Project/thamlam.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <string>
 using namespace std;
 class DoVat
@@ -8,9 +9,16 @@ class DoVat
         float giatri, trongluong;
         int soluong;
         // khởi tạo 3 dối số
+        DoVat();
         DoVat(float giatri, float trongluong, string ten);
         float tinhDonGia(){return giatri/trongluong;}
 };
+DoVat::DoVat()
+{
+    this->ten = "error";
+    this->giatri = 0;
+    this->trongluong = 0;
+}
 DoVat::DoVat(float giatri, float trongluong, string ten)
 {
     this->ten = ten;
@@ -25,12 +33,17 @@ int main()
     a[0] = DoVat(3,4,"A");
     a[1] = DoVat(5,7,"B");
     a[2] = DoVat(10,12,"C");
-    a[3] = DoVat(15,20,"D");
+    a[3] = DoVat(15,25,"D");
     // tính trong lượng
     for(int i = 0; i < 4; i++)
     {
         dongia[i] = a[i].tinhDonGia();
     }   
+    sort(dongia,dongia + 4);
+    for(int i = 3; i >= 0; i--)
+    {
+        cout<<dongia[i]<<" ";
+    }
     // for(int i = 0; i < 4; i++)
     // {
     //     if(tongkg+a[i].trongluong > kg)
