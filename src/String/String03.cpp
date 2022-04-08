f3451787b9335a36d39bc08248c099f369efa48f
nguyentandata3 <nguyentandata3@gmail.com>
2022-04-08 21:43:40 +0700

    sap xep chuoi

--- src/String/String03.cpp
+++ src/String/String03.cpp
@@ -5,6 +5,7 @@
 // - in ra các kí tự là số
 // - kiểm tra 1 số đối xứng không?
 // - kiểm tra tồn tại số chẵn không?
+// - id: M003,M002, M004,.. -> sắp xếp lại cái id theo Ab
 #include <iostream>
 #include <string>
 using namespace std;
@@ -12,6 +13,11 @@ int main()
 {
     string chuoi="22223";
     string chuoi2;
+    string chuoi3[4];
+    chuoi3[0] = "b007";
+    chuoi3[1] = "a004";
+    chuoi3[2] = "g003";
+    chuoi3[3] = "o006";
     bool test = true;
     cout<<chuoi.length()<<"\n";
     cout<<"In chuoi nguoc: ";
@@ -64,5 +70,22 @@ int main()
         }
     }
     cout<<"\nChuoi toan so chan: "<<test;
+    cout<<"\nsap xep chuoi: ";
+    for(int i = 0; i < size-1; i++)
+    {
+        for(int j = 0; j < size; j++)
+        {
+            if(chuoi3[i] < chuoi3[j])
+            {
+                string temp = chuoi3[j];
+                chuoi3[j] = chuoi3[i];
+                chuoi3[i] = temp;
+            }
+        }
+    }
+    for(int i = 0; i < 4; i++)
+    {
+        cout<<chuoi3[i]<<" ";
+    }
     return 0;
 }
